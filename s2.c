4ddea0129a188865b820a6231382f942725c10a2
jams10 <52204522+jams10@users.noreply.github.com>
2020-05-30 19:57:59 +0900

    Create s2.c

---
+++ s2.c
@@ -0,0 +1,203 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<pthread.h>
+#include<time.h>
+
+#define BUF_SIZE 100
+#define MAX_CLIENT 5
+#define MAX_IP 30
+
+FILE* fp;
+#define filename "EK57873.txt"
+
+typedef struct User
+{
+    int sockfd;
+    int id;
+    struct sockaddr_in address;
+}User;
+
+User users[MAX_CLIENT];
+
+void* handle_clnt( void* arg );
+void send_msg( char* msg, int len, int id );
+void error_handling( char* msg );
+char* serverState( int count );
+void menu( char port[] );
+
+
+/****************************/
+
+int clnt_cnt = 0;
+int clnt_socks[MAX_CLNT];
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+int main( int argc, char* argv[] )
+{
+    int serv_sock, clnt_sock;
+    struct sockaddr_in serv_adr, clnt_adr;
+    int clnt_adr_sz;
+    pthread_t t_id;
+
+    /* open file */
+    if( (fp = fopen( filename, "r" )) == NULL )
+    {
+        return(-1);
+    }
+
+    /** time log **/
+    struct tm* t;
+    time_t timer = time( NULL );
+    t = localtime( &timer );
+
+    if( argc != 2 )
+    {
+        printf( " Usage : %s <port>\n", argv[0] );
+        exit( 1 );
+    }
+
+    menu( argv[1] );
+
+    serv_sock = socket( PF_INET, SOCK_STREAM, 0 );
+
+    memset( &serv_adr, 0, sizeof( serv_adr ) );
+    serv_adr.sin_family = AF_INET;
+    serv_adr.sin_addr.s_addr = htonl( INADDR_ANY );
+    serv_adr.sin_port = htons( atoi( argv[1] ) );
+
+    if( bind( serv_sock, (struct sockaddr*)&serv_adr, sizeof( serv_adr ) ) == -1 )
+        error_handling( "bind() error" );
+    if( listen( serv_sock, 5 ) == -1 )
+        error_handling( "listen() error" );
+
+    while( 1 )
+    {
+        t = localtime( &timer );
+        clnt_adr_sz = sizeof( clnt_adr );
+        clnt_sock = accept( serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz );
+
+        pthread_mutex_lock( &mutex );
+            users[clnt_cnt].id = clnt_cnt;
+            users[clnt_cnt].sockfd = clnt_sock;
+            clnt_cnt++;
+        pthread_mutex_unlock( &mutex );
+
+        pthread_create( &t_id, NULL, handle_clnt, (void*)&users[clnt_cnt] );
+        pthread_detach( t_id );
+        //printf( " Connceted client IP : %s ", inet_ntoa( clnt_adr.sin_addr ) );
+        //printf( "(%d-%d-%d %d:%d)\n", t->tm_year + 1900, t->tm_mon + 1, t->tm_mday,
+        //    t->tm_hour, t->tm_min );
+        //printf( " chatter (%d/100)\n", clnt_cnt );
+    }
+    close( serv_sock );
+    return 0;
+}
+
+void* handle_clnt( void* arg )
+{
+    User user = *((User*)arg);
+    int str_len = 0;
+    char msg[BUF_SIZE];
+
+    while( (str_len = read( user.sockfd, msg, sizeof( msg ) )) != 0 )
+    {
+        search( msg );
+        send_msg( msg, str_len, user.id );
+    }
+
+    // remove disconnected client
+    pthread_mutex_lock( &mutex );
+    for( int i = 0; i < clnt_cnt; i++ )
+    {
+        if( user.sockfd == users[i].sockfd )
+        {
+            while( i++ < clnt_cnt - 1 )
+                users[i].sockfd = users[i+1].sockfd;
+            break;
+        }
+    }
+    clnt_cnt--;
+    pthread_mutex_unlock( &mutex );
+    close( user.sockfd );
+    return NULL;
+}
+
+void send_msg( char* msg, int len, int id)
+{
+    int i;
+    pthread_mutex_lock( &mutex );
+    for( i = 0; i < clnt_cnt; i++ )
+    {
+        if( id == users[i].id )
+        {
+            write( users[i].sockfd, msg, len );
+        }
+    }
+    pthread_mutex_unlock( &mutex );
+}
+
+void error_handling( char* msg )
+{
+    fputs( msg, stderr );
+    fputc( '\n', stderr );
+    exit( 1 );
+}
+
+char* serverState( int count )
+{
+    char* stateMsg = malloc( sizeof( char ) * 20 );
+    strcpy( stateMsg, "None" );
+
+    if( count < 5 )
+        strcpy( stateMsg, "Good" );
+    else
+        strcpy( stateMsg, "Bad" );
+
+    return stateMsg;
+}
+
+void menu( char port[] )
+{
+    system( "clear" );
+    printf( " **** moon/sun chat server ****\n" );
+    printf( " server port    : %s\n", port );
+    printf( " server state   : %s\n", serverState( clnt_cnt ) );
+    printf( " max connection : %d\n", MAX_CLIENT );
+    printf( " ****          Log         ****\n\n" );
+}
+
+char* search( char* word )
+{
+    char* str = NULL;
+    char temp[256];
+    int count = 0;
+    int isIn = 0;
+
+    pthread_mutex_lock( &mutex );
+    while( fgets( temp, 256, fp ) != NULL )
+    {
+        if( (strstr( temp, word )) != NULL )
+        {
+            if( strncmp( temp, word, strlen( word ) ) == 0 && temp[0] == word[0] )
+            {
+                rewind( fp );
+                strcpy( str, temp );
+                isIn = 1;
+            }
+            count++;
+        }
+    }
+    strcpy( str, "There is no word.\n" );
+    pthread_mutex_unlock( &mutex );
+
+    if( isIn )
+    {
+        return str;
+    }
+    else return NULL;
+}
