13cd4b8eceacef5d9896da579648a0bac359e577
jams10 <52204522+jams10@users.noreply.github.com>
2020-06-01 11:32:21 +0900

    Update s2.c

--- s2.c
+++ s2.c
@@ -8,16 +8,17 @@
 #include <wchar.h>
 #include <locale.h>
 
-#define MAX_CLIENTS 5
+#define MAX_SERVICE_SOCKETS 5
 #define MAX_BUF 512
 
 #define filename "EK57873.txt"
+/* Text file is from http://www.114pda.com/language/dic-data/dic-ek-57873.htm */
 FILE* fp;
 
-void* service( void* data );
+void* service( void* arg );
 void search( char* word, char msg[] );
 
-int client_index = 0;
+int doing_service_sockets = 0;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 int main( int argc, char** argv )
@@ -33,19 +34,24 @@ int main( int argc, char** argv )
     /* open file */
     if( (fp = fopen( filename, "r" )) == NULL )
     {
+	printf("ERROR : Can't open a file.\n");
         return(-1);
     }
     
-    struct sockaddr_in serv_addr, clnt_addr;
+    struct sockaddr_in serv_addr, client_addr;
+    int client_addr_len = sizeof( client_addr );
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons( atoi( argv[1] ) );
 	
-
-    int server_sock, client_sock;
-    pthread_t clients_threads[MAX_CLIENTS];
-
+    int server_sock, service_sock;
+    pthread_t service_threads[MAX_SERVICE_SOCKETS];
+    
+    // Socket()
     if( (server_sock = socket( AF_INET, SOCK_STREAM, IPPROTO_TCP )) < 0 )
     {
-        printf( "socket create error\n" );
-        return -1;
+        printf( "ERROR : Can't create a socket.\n" );
+        exit(1);
     }
     /*
     int on = 1;
@@ -55,57 +61,55 @@ int main( int argc, char** argv )
         return -1;
     }
     */
-    struct sockaddr_in server_addr, client_addr;
-    int client_addr_size = sizeof( client_addr );
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons( atoi( argv[1] ) );
-
+    
+    // Bind()
     if( bind( server_sock, (struct sockaddr*)&server_addr, sizeof( server_addr ) ) < 0 )
     {
-        printf( "bind error\n" );
-        return -1;
+        printf( "ERROR : Can't bind a socket.\n" );
+        exit(1);
     }
-
+    
+    // Listen()
     if( listen( server_sock, 5 ) < 0 )
     {
-        printf( "listen error\n" );
-        return -1;
+        printf( "ERROR : Can't listen a client socket.\n" );
+        exit(1);
     }
-
+    
+    // Accpet() and provice a service to the clinet.
     while( 1 )
     {
+        printf( "Waiting for a client connection request...\n" );
 
-        printf( "accept...\n" );
-
-        client_sock = accept( server_sock, (struct sockaddr*)&client_addr, (socklen_t*)&client_addr_size );
-        if( client_sock < 0 )
+        service_sock = accept( server_sock, (struct sockaddr*)&client_addr, (socklen_t*)&client_addr_size );
+        
+	if( service_sock < 0 )
         {
-            printf( "accept error\n" );
+            printf( "ERROR : Can't accpet client's request.\n" );
         }
 
-        if( client_index == MAX_CLIENT )
+        if( doing_service_sockets == MAX_SERVICE_SOCKETS )
         {
-            printf( "client accept full(max client count : %d)\n", MAX_CLIENT );
-            close( client_sock );
+            printf( "Can't accpet your request. The maximumm number of service sockets is %d\n", MAX_SERVICE_SOCKETS );
+            close( service_sock );
             continue;
         }
 
-        if( pthread_create( &thread_client[client_index], NULL, t_function, (void*)&client_sock ) != 0 )
+        if( pthread_create( &thread_client[doing_service_sockets++], NULL, &service, (void*)&service_sock ) != 0 )
         {
-            printf( "Thread create error\n" );
-            close( client_sock );
+            printf( "ERROR : Can't create a thread.\n" );
+            close( service_sock );
             continue;
         }
+	// edited : service -> &service
+        //client_index++;
 
-        client_index++;
-
-        printf( "client accepted(Addr: %s, Port: %d)\n", inet_ntoa( client_addr.sin_addr ), ntohs( client_addr.sin_port ) );
+        printf( "(Addr: %s, Port: %d)\n", inet_ntoa( client_addr.sin_addr ), ntohs( client_addr.sin_port ) );
 
     }
 
     fclose( fp );
-    close( client_sock );
+    close( service_sock );
 
     return 0;
 
@@ -113,38 +117,47 @@ int main( int argc, char** argv )
 
 void* t_function( void* arg )
 {
-    int client_sock = *((int*)arg);
+    int service_sock = *((int*)arg);
     pid_t pid = getpid();      // process id
     pthread_t tid = pthread_self();  // thread id
 
     printf( "pid:%u, tid:%x\n", (unsigned int)pid, (unsigned int)tid );
 
-    char buf[BUF_SIZE];
-    char buf_out[BUF_SIZE];
+    char buf[MAX_BUF];
+    //char buf_out[BUF_SIZE];
 
     while( 1 )
     {
-        memset( buf, 0x00, sizeof( buf ) );
-        if( read( client_sock, buf, sizeof( buf ) ) <= 0 )
+        memset( buf, 0, sizeof( buf ) );
+        
+	if( read( service_sock, buf, sizeof( buf ) ) <= 0 )
         {
-            printf( "Client %d close\n", client_sock);
-            client_index--;
-            close( client_sock );
+            printf( "Close service socket (number : %d)\n", service_sock);
+	    
+	    pthread_mutex_lock( &mutex );
+            doing_service_sockets--;
+	    pthread_mutex_unlock( &mutex );
+            
+            close( service_sock );
             break;
         }
 
-        printf( "read : %s\n", buf );
+        printf( "From client : %s\n", buf );
         search( buf, buf );
 
-        if( write( client_sock, buf, sizeof( buf ) ) <= 0 )
+        if( write( service_sock, buf, sizeof( buf ) ) <= 0 )
         {
-            printf( "Client %d close\n", client_sock);
-            client_index--;
-            close( client_sock );
+            printf( "Close service socket (number : %d)\n", service_sock);
+            
+	    pthread_mutex_lock( &mutex );
+	    client_index--;
+	    pthread_mutex_unlock( &mutex );
+            
+	    close( service_sock );
             break;
         }
 	//memset(buf_out, 0x00, sizeof(buf_out));
-        printf("write : %s\n", buf);
+        printf( "To client : %s\n", buf );
     }
 
 }
@@ -152,12 +165,12 @@ void* t_function( void* arg )
 void search( char* word, char msg[] )
 {
     char temp[256];
-    int count = 0;
     int isIn = 0;
 
     strcat( word, " ///" );
 
     pthread_mutex_lock( &mutex );
+	
     while( fgets( temp, 256, fp ) != NULL )
     {
         if( (strstr( temp, word )) != NULL )
@@ -167,13 +180,13 @@ void search( char* word, char msg[] )
                 strcpy( msg, temp );
                 isIn = 1;
             }
-            count++;
         }
     }
-    rewind( fp );
+    rewind( fp ); // Move the file pointer to the start point.
     if(isIn = 0)
     {
 	    strcpy( msg, "There is no word.\n" );
     }
+	
     pthread_mutex_unlock( &mutex );
 }
