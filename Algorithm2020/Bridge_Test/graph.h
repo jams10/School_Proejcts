d57aca745d8aea6c2318f51b7032195571b795c5
jams10 <52204522+jams10@users.noreply.github.com>
2020-11-20 11:59:34 +0900

    Update graph.h

--- Algorithm2020/Bridge_Test/graph.h
+++ Algorithm2020/Bridge_Test/graph.h
@@ -14,14 +14,23 @@ class List
 	Node* tail;
 public:
 	Node* CreateNode( int index );
-	void InsertNode( Node* node);
-	bool Search( int index );
+  Node* GetHead();
+	void InsertNode( Node* node );
+  void ShowList() const;
+  std::vector<int> GetValues() const;
+	bool Search( int index ) const;
+  ~List();
 };
 
 class Graph
 {
 	int nVerts;
-	std::vector<List> vertices;
+	std::vector<List*> vertices;
 public:
-	void SetNumberOfVertices( int n ); 
+  Graph( int nVerts_in );
+	void ShowVertices() const;
+  void ShowConnections() const;
+  int GetSize() const;
+  bool SetConnection( const int& v1, const int& v2 );
+  bool CheckConnection( const int& v1, const int& v2 ) const;
 };
