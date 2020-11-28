099b2d63f544d933158c8e430ab983d75753d16a
jams10 <52204522+jams10@users.noreply.github.com>
2020-11-28 23:06:48 +0900

    Update obst.cpp

--- Algorithm2020/OBST/obst.cpp
+++ Algorithm2020/OBST/obst.cpp
@@ -18,17 +18,18 @@ int main()
 	Tree obst;                // result obst
 
 	SetTable( p, q, w, c, r );
-	ShowTable<float>( w, "Weight Table");
+	ShowTable<float>( w, "Weight Table" );
 	ShowTable<float>( c, "Cost Table" );
 	ShowTable<int>( r, "Root Table" );
 
 	obst.SetRoot( obst.Make( r, 0, 7 ) ); // 루트 테이블로 OBST 만들고 root 노드 설정
 	std::cout << "--------------------Print tree by preorder--------------------" << std::endl << std::endl;
-	obst.Preorder(obst.GetRoot()); // 전위 순회로 트리 출력
+	obst.Preorder( obst.GetRoot() ); // 전위 순회로 트리 출력
 }
 
 void SetTable( const float p[], const float q[], float w[][8], float c[][8], int r[][8] )
 {
+
 	for( int i = 0; i < 8; ++i )
 	{
 		w[i][i] = q[i];
@@ -37,10 +38,17 @@ void SetTable( const float p[], const float q[], float w[][8], float c[][8], int
 	{
 		for( int i = 0; i < 8 - x; ++i )
 		{
+			/*
+				<COST 테이블 값을 구하는 아래의 공식을 그대로 구현>
+				c[i,j] = min( i < k <= j ) { c[i, k-1] + c[k, j] } + w[i, j]
+			*/
 			int j = i + x;
+			// k = i + 1 일 때
 			w[i][j] = w[i][j - 1] + p[j] + q[j];
 			c[i][j] = c[i][i] + c[i + 1][j]; // i < k <= j
 			r[i][j] = i + 1;
+			// k = i + 2 부터 j 까지 반복하면서 최소값 찾기
+			// 최소 COST 값을 만드는 k 값을 루트 테이블에 넣어줌.
 			for( int k = i + 2; k <= j; ++k )
 			{
 				if( c[i][j] > ( c[i][k - 1] + c[k][j] ) )
@@ -49,6 +57,7 @@ void SetTable( const float p[], const float q[], float w[][8], float c[][8], int
 					r[i][j] = k;
 				}
 			}
+			// WEIGHT 값도 더하여 해당 위치의 최종 COST 값 할당.
 			c[i][j] = c[i][j] + w[i][j];
 		}
 	}
